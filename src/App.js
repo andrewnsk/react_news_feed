c4de7e78762c20494c7cccce7776f9eb186ee9c7
smartgo <andrew@dorokhin.moscow>
2018-02-25 21:31:30 +0700

    add form & some bootstrap classes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import LightHeader from './Components/LightHeader'
 import DarkNavBar from './Components/DarkNavBar'
 import List from './Components/ArticleList'
+import Form from './Components/Form'
 
 class App extends Component {
   render() {
@@ -11,7 +12,17 @@ class App extends Component {
       <div className="container">
           <LightHeader/>
           <DarkNavBar/>
-          <List/>
+          <div className="row">
+              <div className="col-md-8 blog-main">
+                  <List/>
+              </div>
+
+              <aside className="col-md-4 blog-sidebar">
+                  <div className="p-3 mb-3 bg-light rounded">
+                      <Form/>
+                  </div>
+              </aside>
+          </div>
       </div>
     );
   }
