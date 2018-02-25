000c22c732f1fdc951d4c381e2433507c3370fd7
smartgo <andrew@dorokhin.moscow>
2018-02-25 12:41:12 +0700

    add Provider with store

--- src/index.js
+++ src/index.js
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from "./store/index";
+import { Provider } from "react-redux";
 import 'bootstrap/dist/css/bootstrap.css';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 registerServiceWorker();
