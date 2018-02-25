276e53abf9b13cb71a3f5951f909db2b093e9966
smartgo <andrew@dorokhin.moscow>
2018-02-25 14:35:21 +0700

    add lorem ipsum text

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,19 @@
 import { ADD_ARTICLE } from "../constants/actionTypes";
 const initialState = {
-    articles: []
+    articles: [
+        {'author': 'Alice', 'time': 'February 24, 2018', 'title': 'Sample blog post',
+            'text': 'Cum sociis natoque penatibus et magnis dis parturient montes,' +
+            ' nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem ' +
+            'lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. ' +
+            'Cras mattis consectetur purus sit amet fermentum.\n' +
+            '\n'},
+        {'author': 'Bob', 'time': 'February 25, 2018', 'title': 'Another blog post',
+            'text': 'Cum sociis natoque penatibus et magnis dis parturient montes, ' +
+            'nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem ' +
+            'lacinia quam venenatis vestibulum. Sed posuere consectetur est at ' +
+            'lobortis. Cras mattis consectetur purus sit amet fermentum.'
+        }
+            ]
 };
 
 const rootReducer = (state = initialState, action) => {
