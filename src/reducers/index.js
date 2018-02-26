1e94b3d19a92be76f7bfe1292d9403c590d51ec4
Andrew Dorokhin <andrew@dorokhin.moscow>
2018-02-26 11:33:38 +0700

    move initialState to store/index.js && add localStorage save/load

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,25 +1,6 @@
 import { ADD_ARTICLE } from "../constants/actionTypes";
 
-const time = (new Date(Date.now()-(new Date()).getTimezoneOffset() * 60000)).toISOString().slice(0, 17).replace('T', ' ');
-
-const initialState = {
-    articles: [
-        {'id': 0, 'author': 'Alice', 'time': time, 'title': 'Sample blog post',
-            'text': 'Cum sociis natoque penatibus et magnis dis parturient montes,' +
-            ' nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem ' +
-            'lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. ' +
-            'Cras mattis consectetur purus sit amet fermentum.\n' +
-            '\n'},
-        {'id': 1, 'author': 'Bob', 'time': time, 'title': 'Another blog post',
-            'text': 'Cum sociis natoque penatibus et magnis dis parturient montes, ' +
-            'nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem ' +
-            'lacinia quam venenatis vestibulum. Sed posuere consectetur est at ' +
-            'lobortis. Cras mattis consectetur purus sit amet fermentum.'
-        }
-            ]
-};
-
-const rootReducer = (state = initialState, action) => {
+const rootReducer = (state, action) => {
     switch (action.type) {
         case ADD_ARTICLE:
             return { ...state, articles: state.articles.concat(action.payload) };
--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,33 @@
 import { createStore } from "redux";
 import rootReducer from "../reducers/index";
-const store = createStore(rootReducer);
+
+const time = (new Date(Date.now()-(new Date()).getTimezoneOffset() * 60000)).toISOString().slice(0, 17).replace('T', ' ');
+
+const initialState = {
+    articles: [
+        {'id': 0, 'author': 'Alice', 'time': time, 'title': 'Sample blog post',
+            'text': 'Cum sociis natoque penatibus et magnis dis parturient montes,' +
+            ' nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem ' +
+            'lacinia quam venenatis vestibulum. Sed posuere consectetur est at lobortis. ' +
+            'Cras mattis consectetur purus sit amet fermentum.\n' +
+            '\n'},
+        {'id': 1, 'author': 'Bob', 'time': time, 'title': 'Another blog post',
+            'text': 'Cum sociis natoque penatibus et magnis dis parturient montes, ' +
+            'nascetur ridiculus mus. Aenean eu leo quam. Pellentesque ornare sem ' +
+            'lacinia quam venenatis vestibulum. Sed posuere consectetur est at ' +
+            'lobortis. Cras mattis consectetur purus sit amet fermentum.'
+        }
+            ]
+};
+
+// const storageState = localStorage.setItem('reduxState', JSON.stringify(initialState));
+
+const persistedState = localStorage.getItem('reduxState') ? JSON.parse(localStorage.getItem('reduxState')) : initialState;
+const store = createStore(rootReducer, persistedState);
+
+store.subscribe(()=>{
+  localStorage.setItem('reduxState', JSON.stringify(store.getState()))
+})
+
+
 export default store;
