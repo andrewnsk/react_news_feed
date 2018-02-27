2a729ecaa4bbe9465d6f23a9b04dd9df072c535e
Andrew Dorokhin <andrew@dorokhin.moscow>
2018-02-27 17:41:02 +0700

    add remove button

--- src/Components/ArticleList.jsx
+++ src/Components/ArticleList.jsx
@@ -1,24 +1,46 @@
-import React from "react";
+import React, {Component} from "react";
 import { connect } from "react-redux";
+import { Button } from 'reactstrap';
+import { removeArticle } from "../actions/index";
+
+
 const mapStateToProps = state => {
     return { articles: state.articles };
 };
-const ConnectedList = ({ articles }) => (
-    <div>
-        <h3 className="pb-3 mb-4 font-italic border-bottom">
-            Hot news
-        </h3>
-        {articles.map(el => (
-            <div className="blog-post" key={el.id}>
-                <h2 className="blog-post-title">{el.title}</h2>
-                <p className="blog-post-meta">{ el.time } by <a href={ "/users/" + el.author }>{ el.author }</a></p>
 
-                <p>{ el.text }</p>
 
+class ConnectedList extends Component {
+
+    buttonClicked(el) {
+        removeArticle(el.id);
+        console.log('click id: ' + el.id)
+    }
+
+      handleClick = (id) => {
+        console.log('this is:', id);
+      }
+
+    render() {
+        return (
+            <div>
+                <h3 className="pb-3 mb-4 font-italic border-bottom">
+                    Hot news
+                </h3>
+                {console.log(this.props)}
+                {this.props.articles.map(el => (
+                    <div className="blog-post" key={el.id}>
+                        <h2 className="blog-post-title">{el.title}</h2>
+                        <p className="blog-post-meta">{ el.time } by <a href={ "/users/" + el.author }>{ el.author }</a></p>
+
+                        <p>{ el.text }</p>
+                        <Button color="danger" onClick={this.handleClick.bind(this, el.id)}>Remove</Button>
+                    </div>
+                ))}
             </div>
-        ))}
-    </div>
-);
+        );
+    }
+}
+
 const List = connect(mapStateToProps)(ConnectedList);
 export default List;
 
