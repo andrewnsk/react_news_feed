1eeec673817e1b2232ef158d8f782d97612ca9e3
Andrew Dorokhin <andrew@dorokhin.moscow>
2018-02-27 17:41:19 +0700

    add REMOVE_ARTICLE reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,14 @@
-import { ADD_ARTICLE } from "../constants/actionTypes";
+import { ADD_ARTICLE, REMOVE_ARTICLE } from "../constants/actionTypes";
 
 const rootReducer = (state, action) => {
     switch (action.type) {
         case ADD_ARTICLE:
+            console.log(action.payload);
             return { ...state, articles: state.articles.concat(action.payload) };
+
+        case REMOVE_ARTICLE:
+            return { ...state };
+
         default:
             return state;
     }
